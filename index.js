f149894a999914fe8969f93a3778db73f1ddecc5
namanhishere <53372786+namanhishere@users.noreply.github.com>
2021-07-15 20:23:59 +0700

    fixing trính tã

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ function getData(params) {
             csv({ noheader: false })
                 .fromString(response.data)
                 .then((jsonObj) => {
-                    let nowtime = new Date()
+                    let nowtime = new Date() 
                     client.channels.cache.get("865175546640072745").send(`[${nowtime.getHours()}:${nowtime.getMinutes()}:${nowtime.getSeconds()}][${nowtime.getFullYear() + "/" + (nowtime.getMonth() + 1) + "/" + nowtime.getDate()}] Making Request Success`)
                     let day = jsonObj.find(({ day_full }) => day_full === nowtime.getFullYear() + "/" + (nowtime.getMonth() + 1) + "/" + nowtime.getDate())
                     if (!fs.existsSync("./data.json")) return fs.writeFileSync('data.json', JSON.stringify(day))
                     let old_data = require("./data.json")
                     if (old_data["CỘNG ĐỒNG"] == day["CỘNG ĐỒNG"]) return
                     fs.writeFileSync('data.json', JSON.stringify(day))
-                    let message = `**Cập nhật vào lúc ${nowtime.getHours()} giờ ${nowtime.getMinutes()} phút Ngày ${nowtime.getDate()} Tháng ${(nowtime.getMonth() + 1)} Năm ${nowtime.getFullYear()}**\nViệt Nam ghi nhận tổng cộng **${day["CỘNG ĐỒNG"]}** ca mắc mới tại **${day.city_by_day}** tỉnh thành.\nTrong đó có **${day.blockade}** được phát hiện ngoài cộng động, **${day.community}** được phát hiện trong khi cách ly.\nNhư vậy, tính từ ngày 27/4 cho tới nay, Việt Nam ghi nhận tổng cộng **${day["TỔNG CỘNG ĐỒNG"]}** ca mắc COVID-19. `
+                    let message = `**Cập nhật vào lúc ${nowtime.getHours()} giờ ${nowtime.getMinutes()} phút Ngày ${nowtime.getDate()} Tháng ${(nowtime.getMonth() + 1)} Năm ${nowtime.getFullYear()}**\nViệt Nam ghi nhận tổng cộng **${day["CỘNG ĐỒNG"]}** ca mắc mới tại **${day.city_by_day}** tỉnh thành.\nTrong đó có **${day.community}** được phát hiện ngoài cộng động, **${day.blockade}** được phát hiện trong khu cách ly.\nNhư vậy, tính từ ngày 27/4 cho tới nay, Việt Nam ghi nhận tổng cộng **${day["TỔNG CỘNG ĐỒNG"]}** ca mắc COVID-19. `
                     client.channels.cache.get("865176721807507467").send(message)
                 })
         })
